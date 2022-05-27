f536548bbcad57eb1d265c84d52b50e6697267b3
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-05-27 20:43:12 +0600

    blog question added

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -5,18 +5,27 @@ const Blog = () => {
         <div>
             <div className='container p-5 pb-5'>
                 <div className='col-md'>
-                    <h3>What is the difference between JavaScript and Node.js?</h3>
-                    <p><strong>JavaScript</strong> <br />
-                        is Lightweight, interpreted, object-oriented language with first-class functions. JavaScript is most known as the scripting language for Web pages, but used in many non-browser environments as well such as node.js or Apache CouchDB. It is a prototype-based, multi-paradigm scripting language that is dynamic,and supports object-oriented, imperative, and functional programming styles.<br />
-                        <strong>Node.js </strong> <br />
-                        is A platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.</p>
+                    <h3>How will you improve the performance of a React Application ?
+                    </h3>
+                    <p><strong>React</strong>uses several clever techniques to minimize the number of costly DOM operations required to update the UI. For many applications, using React will lead to a fast user interface without doing much work to specifically optimize for performance. Nevertheless, there are several ways you can speed up your React application.</p><br />
                 </div>
-
                 <div className='col-md'>
-                    <h3>What is the difference between SQL and NoSQL?</h3>
-                    <p><strong>SQL</strong> is Structured Query Language is a language that is used to have communication between the user and the database. It is used in the databases that use some structured data. Relational databases use SQL for making changes in their databases.<br />
-                        <strong>NoSQL</strong> is "Not Only SQL" or "Not SQL" is used in that type of databases where there is no requirement of a fixed or structured database. Here there is no fixed schema. It is generally used in real-time web applications. Some of the companies that are using NoSQL are Facebook, Google, etc.<br />
+                    <h3>What are the different ways to manage a state in a React application ?</h3>
+                    <p><strong> React</strong>makes use of an observable item because the nation that observes what modifications are made to the kingdom and allows the factor behave as a result. For example, if we replace the kingdom of any element just like the following the web site will no longer re-render itself due to the fact React State will not be able to discover the modifications made.Props are used to skip facts from one component to another. The state is a local statistics garage this is nearby to the component handiest and cannot be exceeded to different components.
                     </p>
+                    <br />
+                </div>
+                <div className='col-md'>
+                    <h3>How does prototypical inheritance work ?</h3>
+                    <p><strong> React</strong>The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the <strong>Prototype</strong> of an object, we use Object. getPrototypeOf and Object.
+                    </p>
+                    <br />
+                </div>
+                <div className='col-md'>
+                    <h3>What is a unit test? Why should write unit tests ?</h3>
+                    <p>Unit testing is a type of software testing where individual units or software components are tested. Its purpose is to validate that each unit of code performs as expected. A unit can be anything you want it to be — a line of code, a method, or a class.The more effort I put into testing the product conceptually at the start of the process, the less effort I had to put into manually testing the product at the end because fewer bugs would emerge as a result.
+                    </p>
+                    <br />
                 </div>
                 <div className='col-md'>
                     <h3>What is the purpose of jwt and how does it work?</h3>
